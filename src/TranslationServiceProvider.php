0f33ff8eab130653040031c76ae7e24b4b252ba6
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-07-16 17:51:50 -0400

    Update the UrlGenerator registration for Laravel 5.6

--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -31,14 +31,8 @@ class TranslationServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->resolved(HttpKernel::class)) {
+        if ($this->app->has(HttpKernel::class)) {
             throw new TranslationException('TranslationServiceProvider should be registered before loading the Kernel');
-        }
-
-        if (! $this->app->has('routes')) {
-            throw new TranslationException(
-                'TranslationServiceProvider should be registered after creating the application'
-            );
         }
 
         $this->mergeConfigFrom(__DIR__.'/../config/translation.php', 'translation');
@@ -66,14 +60,52 @@ class TranslationServiceProvider extends ServiceProvider
      */
     protected function registerUrlGenerator()
     {
-        $this->app->singleton('url', function (Container $app) {
-            $url = new UrlGenerator($app['routes'], $app['request'] ?? null);
+        $this->app->singleton('url', function ($app) {
+            $routes = $app['router']->getRoutes();
 
+            // The URL generator needs the route collection that exists on the router.
+            // Keep in mind this is an object, so we're passing by references here
+            // and all the registered routes will be available to the generator.
+            $app->instance('routes', $routes);
+
+            $url = new UrlGenerator(
+                $routes,
+                $app->rebinding('request', $this->requestRebinder())
+            );
+
+            // Next we will set a few service resolvers on the URL generator so it can
+            // get the information it needs to function. This just provides some of
+            // the convenience features to this URL generator like "signed" URLs.
             $url->setSessionResolver(function () {
                 return $this->app['session'];
             });
 
+            if (is_callable([$url, 'setKeyResolver'])) {
+                $url->setKeyResolver(function () {
+                    return $this->app->make('config')->get('app.key');
+                });
+            }
+
+            // If the route collection is "rebound", for example, when the routes stay
+            // cached for the application, we will need to rebind the routes on the
+            // URL generator instance so it has the latest version of the routes.
+            $app->rebinding('routes', function ($app, $routes) {
+                $app['url']->setRoutes($routes);
+            });
+
             return $url;
         });
+    }
+
+    /**
+     * Get the URL generator request rebinder.
+     *
+     * @return \Closure
+     */
+    protected function requestRebinder()
+    {
+        return function ($app, $request) {
+            $app['url']->setRequest($request);
+        };
     }
 }
