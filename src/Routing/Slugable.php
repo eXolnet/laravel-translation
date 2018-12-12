55e98585fbdda57adf4c61e9b96509bd08ccb99c
Simon Gaudreau <sgaudreau@exolnet.com>
2018-12-12 13:01:01 -0500

    Copy required classes from Exolnet Laravel-module to make Laravel-translation work independently

---
+++ src/Routing/Slugable.php
@@ -0,0 +1,12 @@
+<?php namespace Exolnet\Translation\Routing;
+
+
+interface Slugable
+{
+	/**
+	 * Get the value of the model's route slug.
+	 *
+	 * @return string
+	 */
+	public function getSlug();
+}
