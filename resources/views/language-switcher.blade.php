a4c105c6fde130cccc41e4044462fcfef9029d30
Simon Gaudreau <sgaudreau@exolnet.com>
2018-12-13 14:37:55 -0500

    Fix language switcher blade space after locale code

--- resources/views/language-switcher.blade.php
+++ resources/views/language-switcher.blade.php
@@ -1,7 +1,5 @@
 @forelse (URL::alternates(isset($alternateParameters) ? $alternateParameters : []) as $locale => $url)
-    <a href="{{ $url }}" hreflang="{{ $locale }}">
-        {{$locale}}
-    </a>
+    <a href="{{ $url }}" hreflang="{{ $locale }}">{{$locale}}</a>
 @empty
     @foreach (Route::getAlternateLocales() as $locale)
         <a href="{{ route('home.'. $locale) }}" data-locale="{{ $locale }}" hreflang="{{ $locale }}">
