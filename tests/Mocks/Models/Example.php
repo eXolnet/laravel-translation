e340e7f26663d3c1ed779a470f9530e8b44c759a
Christophe Tremblay <ctremblay@exolnet.com>
2021-01-26 16:42:38 -0500

    Improved Trait/Translatable coverage from 0% to 74%

---
+++ tests/Mocks/Models/Example.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Exolnet\Translation\Tests\Mocks\Models;
+
+use Exolnet\Translation\Traits\Translatable;
+use Illuminate\Database\Eloquent\Model;
+
+class Example extends Model
+{
+    use Translatable;
+
+    /**
+     * @var array
+     */
+    protected $translatedAttributes = ['name', 'description'];
+}
---
+++ tests/Mocks/Models/ExampleTranslation.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Exolnet\Translation\Tests\Mocks\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExampleTranslation extends Model
+{
+    //
+}
